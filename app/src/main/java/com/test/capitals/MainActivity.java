ea88f897d7334ab7bb3c9b079dec6a0900dc541b
balex <balexvicx@gmail.com>
2023-09-02 20:35:58 +0200

    'facebookd'

--- app/src/main/java/com/test/capitals/MainActivity.java
+++ app/src/main/java/com/test/capitals/MainActivity.java
@@ -2,10 +2,15 @@ package com.test.capitals;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Base64;
 import android.util.Log;
-import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,13 +30,26 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 //import com.google.gson.FieldNamingStrategy;
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 import com.google.gson.Gson;
 //import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -117,16 +135,14 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //printHashKey();
 
-        //Log.i("alc",  "MainActivity onCreate");
 
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if (SDK_INT > 8) {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
-            //your codes here
-
         }
         String lineRes = getCountryList();
         ArrayList<CountryDescribe> countryList = new ArrayList<>();
@@ -145,12 +161,6 @@ public class MainActivity extends AppCompatActivity {
             intent.putExtra(EXTRAS_COUNTY_LIST, countryList);
             startActivity(intent);
         } else if ((userName != null) && (!userName.equals(NOT_LOGGED_USER))) {
-//            try {
-//                //Log.i("caps",  "postGetUserScore(userName) 1 : " + postGetUserScore(userName));
-//                postGetUserScore(userName);
-//            } catch (JSONException e) {
-//                throw new RuntimeException(e);
-//            }
             intent = new Intent(this, LoggedUser.class);
             intent.putExtra(EXTRAS_COUNTY_LIST, countryList);
             startActivity(intent);
@@ -158,6 +168,36 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Error userName", Toast.LENGTH_SHORT).show();
         }
     }
+
+//    public void printHashKey()
+//    {
+//        // Add code to print out the key hash
+//        try {
+//            PackageInfo info
+//                    = getPackageManager().getPackageInfo(
+//                    "com.test.capitals",
+//                    PackageManager.GET_SIGNATURES);
+//
+//            for (Signature signature : info.signatures) {
+//
+//                MessageDigest md
+//                        = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:",
+//                        Base64.encodeToString(
+//                                md.digest(),
+//                                Base64.DEFAULT));
+//            }
+//        }
+//
+//        catch (PackageManager.NameNotFoundException e) {
+//        }
+//
+//        catch (NoSuchAlgorithmException e) {
+//        }
+//    }
+
+
 
 
 
--- app/src/main/java/com/test/capitals/NotLoggedUser.java
+++ app/src/main/java/com/test/capitals/NotLoggedUser.java
@@ -9,27 +9,71 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class NotLoggedUser extends AppCompatActivity {
+    private Button mButtonFacebook;
 
+    private CallbackManager callbackManager;
+    private LoginManager loginManager;
     String userName;
     Button buttonLogin, buttonRegister, buttonRules, buttonAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //Log.i("alc",  "NotLoggedUser onCreate");
-
         setContentView(R.layout.not_logged_user);
+
+        printHashKey();
+
+        mButtonFacebook
+                = findViewById(R.id.button_facebook);
+        //FacebookSdk.sdkInitialize(NotLoggedUser.this);
+        callbackManager = CallbackManager.Factory.create();
+        facebookLogin();
+
+        mButtonFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                loginManager.logInWithReadPermissions(
+                        NotLoggedUser.this,
+                        Arrays.asList(
+                                "email",
+                                "public_profile",
+                                "user_birthday"));
+            }
+        });
+
+
         buttonLogin = findViewById(R.id.login);
         buttonRegister = findViewById(R.id.register);
         buttonRules = findViewById(R.id.testRules);
@@ -73,40 +117,142 @@ public class NotLoggedUser extends AppCompatActivity {
 
     }
 
-    @Override
-    public void onStart(){
-        super.onStart();
-        //Log.i("alc",  "NotLoggedUser onStart");
+    public void facebookLogin()
+    {
+
+        loginManager
+                = LoginManager.getInstance();
+        callbackManager
+                = CallbackManager.Factory.create();
+
+        loginManager
+                .registerCallback(
+                        callbackManager,
+                        new FacebookCallback<LoginResult>() {
+
+                            @Override
+                            public void onSuccess(LoginResult loginResult)
+                            {
+                                GraphRequest request = GraphRequest.newMeRequest(
+
+                                        loginResult.getAccessToken(),
+
+                                        new GraphRequest.GraphJSONObjectCallback() {
+
+                                            @Override
+                                            public void onCompleted(JSONObject object,
+                                                                    GraphResponse response)
+                                            {
+
+                                                if (object != null) {
+                                                    try {
+                                                        String name = object.getString("name");
+                                                        String email = object.getString("email");
+                                                        String fbUserID = object.getString("id");
+
+                                                        disconnectFromFacebook();
+
+                                                        // do action after Facebook login success
+                                                        // or call your API
+                                                    }
+                                                    catch (JSONException | NullPointerException e) {
+                                                        e.printStackTrace();
+                                                    }
+                                                }
+                                            }
+                                        });
+
+                                Bundle parameters = new Bundle();
+                                parameters.putString(
+                                        "fields",
+                                        "id, name, email, gender, birthday");
+                                request.setParameters(parameters);
+                                request.executeAsync();
+                            }
+
+                            @Override
+                            public void onCancel()
+                            {
+                                Log.v("LoginScreen", "---onCancel");
+                            }
+
+                            @Override
+                            public void onError(FacebookException error)
+                            {
+                                // here write code when get error
+                                Log.v("LoginScreen", "----onError: "
+                                        + error.getMessage());
+                            }
+                        });
     }
 
-    @Override
-    public void onRestart(){
-        super.onRestart();
-        //Log.i("alc",  "NotLoggedUser onRestart");
+
+    public void disconnectFromFacebook()
+    {
+        if (AccessToken.getCurrentAccessToken() == null) {
+            return; // already logged out
+        }
+
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/me/permissions/",
+                null,
+                HttpMethod.DELETE,
+                new GraphRequest
+                        .Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse graphResponse)
+                    {
+                        LoginManager.getInstance().logOut();
+                    }
+                })
+                .executeAsync();
     }
 
-    @Override
-    public void onResume(){
-        super.onResume();
-        //Log.i("alc",  "NotLoggedUser onResume");
-    }
 
     @Override
-    public void onPause() {
-        super.onPause();
-        //Log.i("alc",  "NotLoggedUser onPause");
+    protected void onActivityResult(int requestCode,
+                                    int resultCode,
+                                    Intent data)
+    {
+
+        // add this line
+        callbackManager.onActivityResult(
+                requestCode,
+                resultCode,
+                data);
+
+        super.onActivityResult(requestCode,
+                resultCode,
+                data);
     }
 
-    @Override
-    public void onStop(){
-        super.onStop();
-        //Log.i("alc",  "NotLoggedUser onStop");
-    }
+        public void printHashKey()
+    {
+        // Add code to print out the key hash
+        try {
+            PackageInfo info
+                    = getPackageManager().getPackageInfo(
+                    "com.test.capitals",
+                    PackageManager.GET_SIGNATURES);
 
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
-        Log.i("alc",  "NotLoggedUser onDestroy");
+            for (Signature signature : info.signatures) {
+
+                MessageDigest md
+                        = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:",
+                        Base64.encodeToString(
+                                md.digest(),
+                                Base64.DEFAULT));
+            }
+        }
+
+        catch (PackageManager.NameNotFoundException e) {
+        }
+
+        catch (NoSuchAlgorithmException e) {
+        }
     }
 
 
